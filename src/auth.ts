d69df5a73af16bc69e4753639c342059171c2198
Jacopo Marrone <jacopo.marrone27@gmail.com>
2024-07-05 15:50:48 +0200

    chore(debug): add log to help track when things happens

--- src/auth.ts
+++ src/auth.ts
@@ -7,23 +7,27 @@ export const { handle, signIn } = SvelteKitAuth({
   providers: [GitHub({
     authorization: { params: { scope: "gist" } }
   })],
+
   callbacks: {
     async jwt({ token, account, user, profile, session, trigger }) {
-      console.log({
-        token, account, user, profile, session, trigger
-      });
+      console.log("auth.js callback => jwt");
+
+      // console.log({
+      //   token, account, user, profile, session, trigger
+      // });
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
     async session({ session, token, user, newSession, trigger }) {
-      console.log({
-        session, token, user, newSession, trigger
-      });
+      console.log("auth.js callback => session");
+      // console.log({
+      //   session, token, user, newSession, trigger
+      // });
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken;
       return session;
-    }
+    },
   }
 }); 
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,22 @@
+import type { Session } from "@auth/sveltekit";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
+  console.log("+layout.server.js => load");
+
   const session = await event.locals.auth();
+  console.log("+layout.server.js => load => session", logSession(session));
 
   return {
     session,
+  };
+};
+
+const logSession = (session: Session | null, logSensitiveInfo = false) => {
+  if (!session) return session;
+  if (logSensitiveInfo) return session;
+  return {
+    user: session.user,
+    expires: session.expires,
   };
 };
